eb7412b7a22d02e315bb8d7beafd4e09a6e9adab
estonec <rusecru@gmail.com>
2021-12-13 11:46:39 +0300

    First commit

---
+++ src/main/java/com/estonianec/hw2dot4/Application.java
@@ -0,0 +1,13 @@
+package com.estonianec.hw2dot4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
